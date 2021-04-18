c8361e8e9b38c1c74530a7dc002ad66efbc8f0da
Prova645 <fabiofattoriw@gmail.com>
2021-04-18 12:58:10 +0200

    Implementazione classe Proprieta,classe Banca e parte della classe Pedina
    classe Pedina non terminata perchè ho bisogno della classe Tabellone che ha implementato Lizzo(la classe Casella non l'ho implementata io ma bensì Lizzo ed è prensente nel mio codice perchè la classe Proprieta eredita dalla classe Casella)

---
+++ MonopolyImplementazioneClassi/Pedina.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonopolyImplementazioneClassi
+{
+    public class Pedina
+    {
+        private int posizione;
+        private float denaroPedina;
+        private List<Proprieta> proprietaCheIlGiocatorePossiede;
+        private List<Proprieta> proprietaIpotecate;
+
+        public Pedina(float denaro)
+        {
+            Posizione = 0;
+        }
+        public int Posizione
+        {
+            get
+            {
+                return posizione;
+            }
+            set
+            {
+                posizione = value;
+            }
+        }
+
+        public float DenaroPedina
+        {
+            get
+            {
+                return denaroPedina;
+            }
+            set
+            {
+                denaroPedina = value;
+                if (Fallisci())
+                    throw new Exception("il giocatore ha fallito");
+            }
+        }
+
+        public List<Proprieta> ProprietaIpotecate
+        {
+            get
+            {
+                return proprietaIpotecate;
+            }
+            set
+            {
+                proprietaIpotecate = value;
+            }
+        }
+
+        public List<Proprieta> ProprietaCheIlGiocatorePossiede
+        {
+            get
+            {
+                return proprietaCheIlGiocatorePossiede;
+            }
+            set
+            {
+                proprietaCheIlGiocatorePossiede = value;
+            }
+        }
+
+        public bool Fallisci()
+        {
+            if (DenaroPedina < 0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public int Giocata()
+        {
+            Random r = new Random();
+
+            int dado1 = r.Next(1, 13);
+            Posizione += dado1;
+            return Posizione;
+        }
+
+        public void Ipoteca()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
